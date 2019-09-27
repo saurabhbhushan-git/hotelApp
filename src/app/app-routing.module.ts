5248cb23f6b0d156e361e3fc34a499e0ed2a243d
Bhushan, Saurabh <saurabh.bhushan@tektronix.com>
2019-09-27 14:08:55 +0530

    added home component add navigation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,19 +8,22 @@ import { WeddingsComponent } from './weddings/weddings.component';
 import { OffersComponent } from './offers/offers.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     { path: 'accomodation', component: AccomodationComponent },
     { path: 'celebrations', component: CelebrationsComponent },
     { path: 'food-beverage', component: FoodBeverageComponent},
-    { path: 'lifestyle', component: LifestyleComponent}, 
-    { path: 'weddings', component: WeddingsComponent}, 
-    { path: 'offers', component: OffersComponent}, 
-    { path: 'signin', component: SigninComponent}, 
-    { path: 'signup', component: SignupComponent}
+    { path: 'lifestyle', component: LifestyleComponent},
+    { path: 'weddings', component: WeddingsComponent},
+    { path: 'offers', component: OffersComponent},
+    { path: 'signin', component: SigninComponent},
+    { path: 'signup', component: SignupComponent},
+    { path: '', component: HomeComponent}
 ];
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes)], 
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,33 @@
-import {NgModule} from '@angular/core';
-import {MatBadgeModule, MatButtonModule} from '@angular/material';
-// import {MatButtonModule} from '@angular/material'
+import { NgModule } from '@angular/core';
+import {
+    MatBadgeModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTabsModule
+} from '@angular/material';
+
 @NgModule({
     imports: [
         MatBadgeModule,
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTabsModule
     ],
     exports: [
-        MatBadgeModule, 
-        MatButtonModule
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTabsModule
     ]
 })
 
-export class MaterialModule{}
+export class MaterialModule { }
